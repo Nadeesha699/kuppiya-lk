20ace08010e1755520affe383c205bddaeea8f77
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2025-12-26 16:30:18 +0530

    saved

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,124 @@
-import Image from "next/image";
+import Link from "next/link";
+import {
+  FiBarChart,
+  FiClock,
+  FiEdit,
+  FiHome,
+  FiMoon,
+  FiSearch,
+  FiSettings,
+  FiShoppingCart,
+  FiUsers,
+} from "react-icons/fi";
 
 export default function Home() {
+  const items = [
+    { name: "Beer", price: 200, stock: 20, category: "Beverages" },
+    { name: "Beer", price: 200, stock: 20, category: "Beverages" },
+    { name: "Beer", price: 200, stock: 20, category: "Beverages" },
+    { name: "Beer", price: 200, stock: 20, category: "Beverages" },
+    { name: "Beer", price: 200, stock: 20, category: "Beverages" },
+    { name: "Beer", price: 200, stock: 20, category: "Beverages" },
+    { name: "Beer", price: 200, stock: 20, category: "Beverages" },
+    { name: "Beer", price: 200, stock: 20, category: "Beverages" },
+  ];
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="w-full h-dvh flex flex-row text-gray-900 font-sans">
+      <div className="w-20 p-5 flex flex-col gap-5 justify-start items-center bg-green-100">
+        <Link href="/">
+          <FiHome
+            size={35}
+            className="bg-green-400 p-2 rounded-lg text-white cursor-pointer"
+          />
+        </Link>
+        <Link href="/edit">
+          <FiEdit
+            size={35}
+            className="bg-transparent p-2 rounded-lg cursor-pointer"
+          />
+        </Link>
+        <Link href="/users">
+          <FiUsers
+            size={35}
+            className="bg-transparent p-2 rounded-lg cursor-pointer"
+          />
+        </Link>
+        <Link href="/analysis">
+          <FiBarChart
+            size={35}
+            className="bg-transparent p-2 rounded-lg cursor-pointer"
+          />
+        </Link>
+        <FiSettings
+          size={35}
+          className="bg-transparent p-2 rounded-lg cursor-pointer"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      </div>
+      <div className="bg-white w-full flex flex-col p-5 gap-5">
+        <div className="flex flex-row justify-end items-end gap-5">
+          <div className="flex flex-row gap-1">
+            <FiClock size={20} />
+            05:30 PM
+          </div>
+          <Link href="/purchase">
+          <FiShoppingCart size={20} /></Link> <FiMoon size={20} />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+        <h1 className="text-6xl font-bold">
+          Mart<span className="text-green-400">X</span>
+        </h1>
+        <div className="flex flex-row gap-5">
+          <div className="flex flex-row gap-2 justify-start items-center bg-gray-100 p-2 rounded-full w-1/2">
+            <FiSearch size={20} />
+            <input
+              placeholder="search by product name..."
+              className="w-full focus:outline-none"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+          <div className="flex flex-row gap-2">
+            <label className="bg-green-400 p-2 rounded-lg text-white flex justify-center items-center cursor-pointer">
+              All
+            </label>
+            <label className="bg-transparent p-2 rounded-lg border-2 border-green-400 text-green-400 flex justify-center items-center cursor-pointer">
+              Beverages
+            </label>
+            <label className="bg-transparent p-2 rounded-lg border-2 border-green-400 text-green-400 flex justify-center items-center cursor-pointer">
+              Sweets
+            </label>
+            <label className="bg-transparent p-2 rounded-lg border-2 border-green-400 text-green-400 flex justify-center items-center cursor-pointer">
+              Gift items
+            </label>
+          </div>
         </div>
-      </main>
+        <div className="w-full h-dvh grid grid-cols-6 gap-5 overflow-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+          {items.map((e, index) => {
+            return (
+              <div className="bg-zinc-100 rounded-lg h-80 p-2 text-sm flex flex-col gap-2" key={index}>
+                <div
+                  className="bg-white rounded-lg w-full h-2/3 relative"
+                  key={index}
+                >
+                  <label className="bg-blue-500 p-1 rounded-lg absolute top-2 left-2 text-white">
+                    {e.category}
+                  </label>
+                </div>
+                <label className="font-bold">{e.name}</label>
+                <div className="flex flex-row gap-2">
+                  <label className="p-1 rounded-full bg-yellow-300 w-1/2 flex justify-center items-center">
+                    LKR {e.price}
+                  </label>
+                  <label className="p-1 rounded-full bg-red-400 w-1/2 flex justify-center items-center text-white">
+                    stock {e.stock}
+                  </label>
+                </div>
+                <div className="flex flex-row gap-2 justify-center items-center border-green-400 rounded-lg p-1 text-green-400 border-2">
+                  <FiShoppingCart />
+                  add
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ app/purchase/page.tsx
@@ -0,0 +1,8 @@
+const Purchase = () =>{
+    return(
+<div className="w-full h-dvh flex flex-row text-gray-900 font-sans bg-white"></div>
+    )
+
+}
+
+export default Purchase
